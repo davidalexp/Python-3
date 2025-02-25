c65d40887ffac3cd6bc20053950c693ab95a2c98
David Alexandre <156386626+davidalexp@users.noreply.github.com>
2025-02-25 18:12:55 -0300

    Update exe022 - analisador de textos.py

--- exe022 - analisador de textos.py
+++ exe022 - analisador de textos.py
@@ -1,5 +1,4 @@
-#crie um programa que leia o nome completo de uma pessoa
-#e mostre: 1) o nome com todas as letras maiúsculas
+#crie um programa que leia o nome completo de uma pessoa e mostre: 1) o nome com todas as letras maiúsculas
 #2) com todas minúsculas
 #3) quantas letras ao todo (sem contar os espaços)
 #4) quantas letras tem o primeiro nome
